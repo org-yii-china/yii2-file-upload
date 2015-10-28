c5443c64c51ee1d1a2186ef9e13ac132104f1552
Huangxianan <xianan_huang@163.com>
2015-10-28 22:01:15 +0800

    1.0.1

---
+++ FileUpload.php
@@ -0,0 +1,54 @@
+<?php
+namespace common\widgets\file_upload;
+
+use Yii;
+use yii\widgets\InputWidget;
+use yii\helpers\Html;
+use yii\web\View;
+use common\widgets\file_upload\assets\FileUploadAsset;
+use yii\helpers\ArrayHelper;
+use yii\helpers\Url;
+
+class FileUpload extends InputWidget
+{
+    public $config = [];
+	public $value = '';
+    
+    public function init()
+    {
+        $_config = [
+            'serverUrl' => Url::to(['upload','action'=>'uploadimage']),  //上传服务器地址
+            'fileName' => 'upfile',                                      //提交的图片表单名称 
+            'domain_url' => '',                                          //图片域名 不填为当前域名
+        ];
+        $this->config = ArrayHelper::merge($_config, $this->config);
+
+    }
+    
+    public function run()
+    {
+        $this->registerClientScript();        
+        if ($this->hasModel()) {
+            $inputName = Html::getInputName($this->model, $this->attribute);
+            $inputValue = Html::getAttributeValue($this->model, $this->attribute);
+            return $this->render('index',[
+                'config'=>$this->config,
+                'inputName' => $inputName,
+                'inputValue' => $inputValue,
+            ]);
+        } else {
+            return $this->render('index',[
+                'config'=>$this->config,
+                'inputName' => 'file-upload',
+				'inputValue' => $this->value,
+            ]);
+        }
+    }
+    
+    public function registerClientScript()
+    {
+        FileUploadAsset::register($this->view);
+        //$script = "FormFileUpload.init();";
+        //$this->view->registerJs($script, View::POS_READY);
+    }
+}
