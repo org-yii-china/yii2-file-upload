6d566dfc0668e0da6e1261b5435848cd7c547a82
Huangxianan <xianan_huang@163.com>
2015-12-27 11:46:02 +0800

    编辑
    编辑显示图片bug

--- views/index.php
+++ views/index.php
@@ -9,7 +9,7 @@
 use yii\helpers\Html;
 ?>
 <div class="per_upload_con" data-url="<?=$config['serverUrl']?>">
-    <div class="per_real_img <?=$attribute?>" domain-url = "<?=$config['domain_url']?>"></div>
+    <div class="per_real_img <?=$attribute?>" domain-url = "<?=$config['domain_url']?>"><?=isset($inputValue)?'<img src="'.$config['domain_url'].$inputValue.'">':''?></div>
     <div class="per_upload_img">图片上传</div>
     <div class="per_upload_text">
         <p class="upbtn" ><a id="<?=$attribute?>" href="javascript:;" class="btn btn-success green choose_btn">选择图片</a></p>
